ec76374251553eb81cd5c8af151183154b708875
Sergey <s.p.sergey@ukr.net>
2021-07-28 18:15:41 +0300

    add package "catchTheexcEptions"

---
+++ src/java/com/skillup/classes/catchTheexcEptions/MaineEx.java
@@ -0,0 +1,9 @@
+package com.skillup.classes.catchTheexcEptions;
+
+public class MaineEx {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/java/com/skillup/classes/catchTheexcEptions/NegativeNumbers.java
@@ -0,0 +1,9 @@
+package com.skillup.classes.catchTheexcEptions;
+
+public class NegativeNumbers extends Exception{
+
+    public NegativeNumbers() {
+        super("This is negative numbers");
+    }
+
+}
