1e8a65f9fe8a027b8a0ba6609b915d884646db58
Sergey <s.p.sergey@ukr.net>
2021-08-18 13:18:01 +0300

    add class - sortingAnArrayOfChar.

---
+++ src/java/com/skillup/classes/sortingAnArrayOfChar/SortingChar.java
@@ -0,0 +1,32 @@
+package com.skillup.classes.sortingAnArrayOfChar;
+
+import java.util.Arrays;
+
+public class SortingChar {
+    public static void main(String[] args) {
+        char[] arr = {'d', 'w', 'g', 'e', 'a', 'h', 'r', 's', 'q', 'b', 'y'};
+
+        bubbleSorting(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static char[] bubbleSorting(char[] a) {
+
+        boolean isSorted = false;
+        while(!isSorted) {
+            isSorted = true;
+            char bufChar;
+            for (int i = 0; i < a.length-1 ; i++) {
+                if (a[i] > a[i + 1]) {
+                    isSorted = false;
+                    bufChar = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = bufChar;
+                }
+            }
+        }
+        return a;
+    }
+}
